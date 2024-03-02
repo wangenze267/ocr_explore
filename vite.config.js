d69ac423ce0632b7fdab3e39fb1f12a73022212e
wangenze267 <15041567431@126.com>
2024-03-02 13:11:53 +0800

    🔥去除无用文件，引入devui

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+import Components from 'unplugin-vue-components/vite'
 
 import { defineConfig } from 'vite'
+import { DevUiResolver } from 'unplugin-vue-components/resolvers'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [
+        DevUiResolver
+      ]
+    })
   ],
   resolve: {
     alias: {
